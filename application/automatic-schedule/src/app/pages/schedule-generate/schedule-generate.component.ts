131b970ae9d4ae7dbf4f95da5e22ed38ac7bf652
haovvv73 <haovvv73@gmail.com>
2024-05-02 06:36:55 +0700

    [feature] in progress schedule generate

--- application/automatic-schedule/src/app/pages/schedule-generate/schedule-generate.component.ts
+++ application/automatic-schedule/src/app/pages/schedule-generate/schedule-generate.component.ts
@@ -1,12 +1,13 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-schedule-generate',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './schedule-generate.component.html',
   styleUrl: './schedule-generate.component.css'
 })
 export class ScheduleGenerateComponent {
-
+  title = 'Schedule Generate'
 }
