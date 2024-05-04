07f6a703b56119b44eb039b0b647dd563a14a775
haovvv73 <haovvv73@gmail.com>
2024-05-04 17:07:21 +0700

    [feature] done layout schedule-generate

--- application/automatic-schedule/src/app/env-url.ts
+++ application/automatic-schedule/src/app/env-url.ts
@@ -6,9 +6,13 @@ export enum EnvUrl {
     lesson_admin = '/app/admin/lesson',
     teacher_admin = '/app/admin/teacher',
     room_admin = '/app/admin/room',
+
     schedule_admin = '/app/admin/schedule',
     scheduleView_admin = '/app/admin/schedule-view/',
+    scheduleGenerate_admin = '/app/admin/schedule-generate/',
+    
     courseView_admin = '/app/admin/course-view/',
+    
     approval_admin = '/app/admin/approval',
     // user
     request_user = '/app/user/request',
