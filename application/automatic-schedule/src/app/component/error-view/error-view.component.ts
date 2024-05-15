c48707465134c91a22414bad35d3f84d99873c2d
giahao73 <votrangiahao73@gmail.com>
2024-05-15 17:03:03 +0700

    [feature] error service

---
+++ application/automatic-schedule/src/app/component/error-view/error-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-error-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './error-view.component.html',
+  styleUrl: './error-view.component.css'
+})
+export class ErrorViewComponent {
+
+}
